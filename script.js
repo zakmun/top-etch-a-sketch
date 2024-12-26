017b413bc79b32d2a30589242fee786995dd550d
zaki <zaki-munshi@hotmail.co.uk>
2024-12-26 14:27:14 +0000

    Create grid to display on webpage

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ const container = document.querySelector('.container');
 
 
 
-function createGrid (grid) {
+function createGrid (numberPerRow) {
 
-    for (let i = 0; i < grid; i++) {
+    
+    const total = (numberPerRow * numberPerRow) + numberPerRow;
+    const mod = numberPerRow + 1;
+
+    for (let i = 0; i <= total; i++) {
     
         const squares = document.createElement('div');
         squares.classList.add('squares')
-        container.appendChild(squares);
         
+        if (i % mod === 0) {
+            squares.style.cssText = "border: 0; height: 0; width: 100%";
+          } else {
+            squares.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+          }
+    container.appendChild(squares);
+
     }
     
 }
 
-createGrid(32)
+createGrid(16)
